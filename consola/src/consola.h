812b704b00d427c48747438b984d5d56c0c10fb8
Alesio <alesiosaba@gmail.com>
2022-05-02 20:25:43 -0300

    creacion de modulos consola y kernel

---
+++ consola/src/consola.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
---
+++ kernel/src/kernel.c
@@ -0,0 +1,6 @@
+#include "kernel.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
