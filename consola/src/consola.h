9b73b10b8f926fb18e724ce341ae9654f4668d77
Alesio <alesiosaba@gmail.com>
2022-05-10 19:40:48 -0300

    consola lee lista de instrucciones

--- consola/src/consola.h
+++ consola/src/consola.h
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<commons/log.h>
-#include<commons/string.h>
-#include<commons/config.h>
+#include <string.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
 
-typedef enum
-{
-	NO_OP,
-    IO,
-	READ,
-    COPY,
-    WRITE,
-    EXIT
-} identificador_instruccion;
 
-typedef struct
+typedef struct nodo_parametro
 {
-	identificador_instruccion identificador;
-    int** parametros;
+	int parametro;
+    struct nodo_parametro* sig;
+} nodo_parametro;
+
+typedef struct instruccion
+{
+	char identificador[10];
+    struct nodo_parametro* parametros;
 } instruccion;
 
-instruccion** armar_lista_instrucciones(char* path_pseudocodigo);
+typedef struct nodo_instruccion
+{
+	instruccion instruccion;
+    struct nodo_instruccion* sig;
+} nodo_instruccion;
+
+void mostrar_lista(nodo_instruccion* lista_instrucciones);
+nodo_instruccion* armar_lista_instrucciones(char* path_pseudocodigo);
+nodo_instruccion* agregar_primera_instruccion(char* buffer);
+void agregar_nueva_instruccion(nodo_instruccion* lista_instrucciones, char* buffer);
+FILE* abrir_archivo_lectura(char* path_pseudocodigo);
+
+nodo_instruccion* nuevo_nodo_instruccion();
+void completar_nodo_instruccion(nodo_instruccion* nodo_instruccion, char* buffer);
+
+nodo_parametro* nuevo_nodo_parametro();
+nodo_parametro* agregar_primer_parametro(char* parametro);
+void agregar_nuevo_parametro(nodo_instruccion* nodo_instruccion, char* parametro);
