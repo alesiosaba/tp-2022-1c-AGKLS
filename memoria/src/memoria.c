e59b721cfe425c3659165cc95c8f85913324311f
karengruschka <karengruschka98@gmail.com>
2022-06-07 21:06:18 -0300

    Memoria lee archivo de configuracion

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -10,8 +10,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/memoria.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	inicializar();
+	terminar_programa(logger, config);
 	return EXIT_SUCCESS;
+}
+
+void terminar_programa()
+{
+	config_destroy(config);
+	log_debug(logger,CONFIGURACION_CERRADA);
+	close(conexion);
+	log_debug(logger,TERMINANDO_EL_LOG);
+	log_destroy(logger);
 }
