38d804ee5cbaa71faedb4190c918fc5b685c8dc8
Alesio <alesiosaba@gmail.com>
2022-07-16 12:25:34 -0300

    script compilacion

---
+++ comandos/compilar.sh
@@ -0,0 +1,31 @@
+echo Compilando modulos...
+
+echo
+echo Compilando shared...
+echo
+cd ../shared
+make clean all
+
+echo
+echo Compilando consola...
+echo
+cd ../consola
+make clean all
+
+echo
+echo Compilando kernel...
+echo
+cd ../kernel
+make clean all
+
+echo
+echo Compilando CPU...
+echo
+cd ../CPU
+make clean all
+
+echo
+echo Compilando Memoria...
+echo
+cd ../memoria
+make clean all
