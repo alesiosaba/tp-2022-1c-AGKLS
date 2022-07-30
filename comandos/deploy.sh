b2fa975d18dfa0db230723f46c3e6f959b0a5d5c
Alesio Saba <alesiosaba@gmail.com>
2022-07-29 22:10:06 -0300

    deploy.sh

---
+++ comandos/deploy.sh
@@ -0,0 +1,53 @@
+echo
+echo Clonando tp-2022-1c-AGKLS...
+echo
+git clone https://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@github.com/sisoputnfrba/tp-2022-1c-AGKLS.git
+
+cd tp-2022-1c-AGKLS
+echo
+echo Clonando commons...
+echo
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library
+echo
+echo Instalando commons...
+echo
+sudo make install
+
+echo
+echo
+echo Compilando modulos...
+echo
+
+echo Compilando shared...
+echo
+cd ../shared
+make clean all
+
+echo
+echo Compilando consola...
+echo
+cd ../consola
+make clean all
+
+echo
+echo Compilando kernel...
+echo
+cd ../kernel
+make clean all
+
+echo
+echo Compilando CPU...
+echo
+cd ../CPU
+make clean all
+
+echo
+echo Compilando Memoria...
+echo
+cd ../memoria
+make clean all
+
+echo
+echo deploy finalizado con exito
+echo
