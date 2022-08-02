0331d20bb1a4992b848a0bf5000cc4ba0dbc590a
Alesio <alesiosaba@gmail.com>
2022-08-02 01:34:48 -0300

    ignore consola/update-config.sh

--- consola/update-config.sh
+++
@@ -1,9 +0,0 @@
-echo
-echo Actualizando consola.config ...
-echo
-
-read -p "Ingrese LOG_LEVEL: " LOG_LEVEL
-grep -RiIl 'LOG_LEVEL' | xargs sed -i 's|\(LOG_LEVEL\s*=\).*|\1'$LOG_LEVEL'|'
-
-read -p "Ingrese IP_KERNEL: " IP_KERNEL
-grep -RiIl 'IP_KERNEL' | xargs sed -i 's|\(IP_KERNEL\s*=\).*|\1'$IP_KERNEL'|'
